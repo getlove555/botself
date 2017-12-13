7d55a616837611237714fdcd68d7cbf0e2e7577f
getlove555 <33900005+getlove555@users.noreply.github.com>
2017-12-13 23:51:49 +0700

    Update and rename LINETCR/Api/channel.py to  LINETCR/ Api/channel.py

--- LINETCR/ Api/channel.py
+++ LINETCR/ Api/channel.py
@@ -19,8 +19,8 @@ class Channel:
     channel_query_path = "/CH4"
 
     UA = "Line/6.0.0 iPad4,1 9.0.2"
-    LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
-
+    LA = "IOSIPAD\t7.14.0\tiPhone OS\t10.12.0"
+    
     authToken = None
     mid = None
     channel_access_token = None
